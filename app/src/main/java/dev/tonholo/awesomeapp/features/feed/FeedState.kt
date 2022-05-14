701dedfecad903bd3468ebdb04da61164e8933bb
Rafael Tonholo <rafael@tonholo.dev>
2022-05-14 15:47:04 -0300

    created the feed screen state

---
+++ app/src/main/java/dev/tonholo/awesomeapp/features/feed/FeedState.kt
@@ -0,0 +1,1 @@
+package dev.tonholo.awesomeapp.features.feedimport dev.tonholo.awesomeapp.data.model.UnsplashImagedata class FeedState(    val isLoading: Boolean = false,    val hasError: Boolean = false,    val errorMessage: String? = null,    val images: List<UnsplashImage> = emptyList(),)
