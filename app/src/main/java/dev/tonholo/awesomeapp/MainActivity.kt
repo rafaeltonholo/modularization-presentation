8a882594845af8a67bd383a85986eb2f18e3d76f
Rafael Tonholo <rafael@tonholo.dev>
2022-05-15 11:25:01 -0300

    added navigation

--- app/src/main/java/dev/tonholo/awesomeapp/MainActivity.kt
+++ app/src/main/java/dev/tonholo/awesomeapp/MainActivity.kt
@@ -3,14 +3,8 @@ package dev.tonholo.awesomeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
+import dev.tonholo.awesomeapp.navigation.Navigation
 import dev.tonholo.awesomeapp.ui.theme.AwesomeAppTheme
 
 @AndroidEntryPoint
@@ -19,27 +13,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AwesomeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Navigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AwesomeAppTheme {
-        Greeting("Android")
     }
 }
