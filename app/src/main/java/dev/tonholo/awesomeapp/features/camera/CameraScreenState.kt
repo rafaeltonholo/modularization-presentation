13151b75de803d6d5b2f5675b2be9850901f91de
Rafael Tonholo <rafael@tonholo.dev>
2022-05-19 19:51:29 -0300

    added camera screen

--- app/src/main/java/dev/tonholo/awesomeapp/features/camera/CameraScreenState.kt
+++ app/src/main/java/dev/tonholo/awesomeapp/features/camera/CameraScreenState.kt
@@ -1,1 +1,1 @@
-package dev.tonholo.awesomeapp.features.cameraimport android.net.Uridata class CameraScreenState(    val uri: Uri? = null,    val isRearCamera: Boolean = false,)
+package dev.tonholo.awesomeapp.features.cameraimport android.net.Uridata class CameraScreenState(    val uri: Uri? = null,    val isRearCamera: Boolean = false,    val errorMessage: String? = null,)
