2349c9528baba78a0ec40ae2d6457b4906405266
Rafael Tonholo <rafael@tonholo.dev>
2022-05-22 23:37:58 +0100

    feat: modularized onboard feature

--- 2. modularized/(Modularized) AwesomeApp/base/src/main/java/dev/tonholo/awesomeapp/MainActivity.kt
+++ 2. modularized/(Modularized) AwesomeApp/base/src/main/java/dev/tonholo/awesomeapp/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
+import dev.tonholo.awesomeapp.navigation.DestinationManager
 import dev.tonholo.awesomeapp.navigation.Navigation
 import dev.tonholo.awesomeapp.ui.theme.AwesomeAppTheme
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var destinationManager: DestinationManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AwesomeAppTheme {
-                Navigation()
+                Navigation(destinationManager)
             }
         }
     }
