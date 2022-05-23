cf99eaaaed90622982a422a883d2a8c7d1d20e02
Rafael Tonholo <rafael@tonholo.dev>
2022-05-23 15:00:02 +0100

    feat: modularized shopping feature

--- 2. modularized/(Modularized) AwesomeApp/app/src/main/java/dev/tonholo/awesomeapp/features/shopping/ShoppingState.kt
+++
@@ -1,1 +0,0 @@
-package dev.tonholo.awesomeapp.features.shoppingimport dev.tonholo.awesomeapp.data.model.UnsplashImagedata class ShoppingState(    val isSearching: Boolean = false,    val waitingFirstInput: Boolean = true,    val images: List<UnsplashImage> = emptyList(),    val filter: String = "",)
