dc212b45e73fa00f335f8b9041aca462bac93737
Rafael Tonholo <rafael@tonholo.dev>
2022-05-23 15:42:05 +0100

    feat: modularized feed feature; created navigation library

--- 2. modularized/(Modularized) AwesomeApp/app/src/main/java/dev/tonholo/awesomeapp/features/feed/FeedState.kt
+++
@@ -1,1 +0,0 @@
-package dev.tonholo.awesomeapp.features.feedimport dev.tonholo.awesomeapp.data.model.UnsplashImagedata class FeedState(    val isLoading: Boolean = false,    val hasError: Boolean = false,    val errorMessage: String? = null,    val images: List<UnsplashImage> = emptyList(),    val destinationTarget: String? = null,)
---
+++ 2. modularized/(Modularized) AwesomeApp/feature/feed/src/main/java/dev/tonholo/awesomeapp/feature/feed/FeedState.kt
@@ -0,0 +1,1 @@
+package dev.tonholo.awesomeapp.feature.feedimport dev.tonholo.awesomeapp.data.model.UnsplashImagedata class FeedState(    val isLoading: Boolean = false,    val hasError: Boolean = false,    val errorMessage: String? = null,    val images: List<UnsplashImage> = emptyList(),    val destinationTarget: String? = null,)
