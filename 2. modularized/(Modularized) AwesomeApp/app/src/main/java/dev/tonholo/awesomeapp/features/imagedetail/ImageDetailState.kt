bf010f7c35fa226858e7653fafdfd38f6b2a5dac
Rafael Tonholo <rafael@tonholo.dev>
2022-05-23 15:15:19 +0100

    feat: modularized imagedetail feature

--- 2. modularized/(Modularized) AwesomeApp/app/src/main/java/dev/tonholo/awesomeapp/features/imagedetail/ImageDetailState.kt
+++
@@ -1,1 +0,0 @@
-package dev.tonholo.awesomeapp.features.imagedetailimport dev.tonholo.awesomeapp.data.model.UnsplashImagedata class ImageDetailState(    val isLoading: Boolean = false,    val image: UnsplashImage? = null,    val errorMessage: String? = null,)
