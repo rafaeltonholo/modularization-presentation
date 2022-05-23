f328b6dee99d184c7d291be01885f84ec52b90e2
Rafael Tonholo <rafael@tonholo.dev>
2022-05-23 14:13:11 +0100

    feat: modularized camera feature

--- 2. modularized/(Modularized) AwesomeApp/app/src/main/java/dev/tonholo/awesomeapp/features/camera/CameraScreenState.kt
+++
@@ -1,1 +0,0 @@
-package dev.tonholo.awesomeapp.features.cameraimport android.net.Uridata class CameraScreenState(    val uri: Uri? = null,    val isRearCamera: Boolean = false,    val errorMessage: String? = null,)
