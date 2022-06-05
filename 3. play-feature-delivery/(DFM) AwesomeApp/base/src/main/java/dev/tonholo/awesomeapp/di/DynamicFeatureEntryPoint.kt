4eaa56610c2ded08209cf15f3b39381f8883b191
Rafael Tonholo <rafael@tonholo.dev>
2022-06-05 18:50:00 -0300

    fix: added application context as a dependency for all dfms

--- 3. play-feature-delivery/(DFM) AwesomeApp/base/src/main/java/dev/tonholo/awesomeapp/di/DynamicFeatureEntryPoint.kt
+++ 3. play-feature-delivery/(DFM) AwesomeApp/base/src/main/java/dev/tonholo/awesomeapp/di/DynamicFeatureEntryPoint.kt
@@ -1,9 +1,14 @@
 package dev.tonholo.awesomeapp.di
 
+import android.content.Context
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @EntryPoint
 @InstallIn(SingletonComponent::class)
-interface DynamicFeatureEntryPoint
+interface DynamicFeatureEntryPoint {
+    @ApplicationContext
+    fun getApplicationContext(): Context
+}
